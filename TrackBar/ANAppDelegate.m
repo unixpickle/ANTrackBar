8b5bd76ab6c72231db223e4e77f26069808279ac
Alex Nichol <alexnichol@comcast.net>
2012-02-28 19:23:42 -0500

    Initial Commit

---
+++ TrackBar/ANAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ANAppDelegate.m
+//  TrackBar
+//
+//  Created by Alex Nichol on 2/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ANAppDelegate.h"
+
+@implementation ANAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
